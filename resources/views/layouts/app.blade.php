5f2ecf11baf4ac9261e60f473caf5994e9414d42
Steve Nyanumba <reach@stevennmb9@gmail.com>
2024-01-30 14:22:13 +0300

    Refined Functionality

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,7 +56,7 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center bg-alps-danger">
-            <img class="" src="/logo_light.png" alt="Logo" height="150" width="150">
+            <img class="" src="/company_logo.png" alt="Logo" height="150" width="150">
         </div>
 
         <!-- Navbar -->
