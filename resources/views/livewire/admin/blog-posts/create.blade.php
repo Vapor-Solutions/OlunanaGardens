21e682a7cbc45cfc751cdf0fa5b1a4ad3be5ad7d
Errol Onzere Kitsiiri <errolantonio325@gmail.com>
2024-03-06 22:52:19 +0300

    Tried resolving blog posts

--- resources/views/livewire/admin/blog-posts/create.blade.php
+++ resources/views/livewire/admin/blog-posts/create.blade.php
@@ -28,6 +28,13 @@
                         <label for="headerPhoto" class="form-label">Header Photo</label>
                         <input type="file" wire:model='headerPhoto' class="form-control" name="headerPhoto"
                             id="headerPhoto" aria-describedby="helpId" placeholder="" />
+                            @if ($headerPhoto)
+                            <div class="row">
+                                <div class="col-3  mt-2 me-1 mb-2 ">
+                                    <img src="{{ $headerPhoto->temporaryUrl() }}" height="200" width="200">
+                                </div>
+                            </div>
+                        @endif
                         @error('headerPhoto')
                             <small id="headerPhoto" class="form-text text-danger">{{ $message }}</small>
                         @enderror
@@ -37,15 +44,24 @@
                 <div class="col-md-6">
                     <div class="mb-3">
                         <label for="blogPhoto" class="form-label">Thumbnail</label>
-                        <input type="file" wire:model='' class="form-control" name="blogPhoto"
-                            id="blogPhoto" aria-describedby="helpId" placeholder="" />
+                        <input type="file" wire:model='blogPhoto' class="form-control" name="blogPhoto" id="blogPhoto"
+                            aria-describedby="helpId" placeholder="" />
+                        @if ($blogPhoto)
+                            <div class="row">
+                                <div class="col-3  mt-2 me-1 mb-2 ">
+                                    <img src="{{ $blogPhoto->temporaryUrl() }}" height="200" width="200">
+                                </div>
+                            </div>
+                        @endif
                         @error('blogPhoto')
                             <small id="blogPhoto" class="form-text text-danger">{{ $message }}</small>
                         @enderror
                     </div>
                 </div>
             </div>
+            <button class="btn btn-primary" wire:click='save'>submit</button>
         </div>
+
     </div>
 </div>
 
