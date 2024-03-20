de9fbb2b92280f62b4f3211bbccd656eae237a4f
Steve Nyanumba <reach@stevenyanumba.com>
2024-03-20 12:17:11 +0300

    Recompiled Assets

--- resources/views/livewire/admin/booking-requests/index.blade.php
+++ resources/views/livewire/admin/booking-requests/index.blade.php
@@ -10,27 +10,44 @@
                 <thead class="">
 
                     <tr>
-                        <th scope="row">ID</th>
+                        <th>ID</th>
                         <th>Date</th>
                         <th>Alias</th>
                         <th>Email Address</th>
+                        <th>Phone Number</th>
                         <th>Type of Event</th>
                         <th>No. of adults</th>
                         <th>No. of children</th>
-                        <th>Actions</th>
+                        <th class="text-center">Actions</th>
                     </tr>
                 </thead>
                 <tbody class="table-group-divider">
 
-                    <tr>
-                        <td scope="row">Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
-                    </tr>
+                    @foreach ($bookingRequests as $request)
+                        <tr>
+                            <td scope="row">{{ $request->id }}</td>
+                            <td>{{ Carbon\Carbon::parse($request->start_time)->format('jS F, Y h:i:sA') }}</td>
+                            <td>{{ $request->client->name }}</td>
+                            <td>{{ $request->client->email }}</td>
+                            <td>{{ $request->client->phone_number }}</td>
+                            <td>{{ $request->eventType->title }}</td>
+                            <td>{{ $request->capacity_adults }}</td>
+                            <td>{{ $request->capacity_children }}</td>
+                            <td class="d-flex flex-row justify-content-center">
+                                <div class="flex-col m-1">
+                                    <button class="btn btn-success" data-toggle="tooltip" title="Make the Booking">
+                                        <i class="fas fa-check"></i>
+                                    </button>
+                                </div>
+
+
+                            </td>
+                        </tr>
+                    @endforeach
+
+
+
+
                 </tbody>
                 <tfoot>
 
