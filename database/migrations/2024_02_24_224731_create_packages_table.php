788b0c4e009543e36aebd3382a0ae7edcef92cdf
Steve Nyanumba <reach@stevenyanumba.com>
2024-03-22 11:38:49 +0300

    Add the Booking Requests

--- database/migrations/2024_02_24_224731_create_packages_table.php
+++ database/migrations/2024_02_24_224731_create_packages_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('packages', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->longText('description');
             $table->unsignedDecimal('price', 8, 2);
             $table->timestamps();
         });
--- resources/views/livewire/admin/booking-requests/index.blade.php
+++ resources/views/livewire/admin/booking-requests/index.blade.php
@@ -35,23 +35,60 @@
                             <td>{{ $request->capacity_children }}</td>
                             <td class="d-flex flex-row justify-content-center">
                                 <div class="flex-col m-1">
-                                    <button class="btn btn-success" data-toggle="tooltip" title="Make the Booking">
+                                    <!-- Modal trigger button -->
+                                    <button type="button" class="btn btn-primary btn" data-toggle="modal"
+                                        data-target="#modalId{{ $request->id }}">
                                         <i class="fas fa-check"></i>
                                     </button>
+
+                                    <!-- Modal Body -->
+                                    <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-backdrop and data-keyboard -->
+                                    <div class="modal fade" wire:ignore id="modalId{{ $request->id }}" tabindex="-1"
+                                        data-backdrop="static" data-keyboard="false" role="dialog"
+                                        aria-labelledby="modalTitleId" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-xl"
+                                            role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="modalTitleId">
+                                                        Make Booking for Request No. #{{ $request->id }}
+                                                    </h5>
+                                                    <button type="button" class="btn-close" data-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div class="mb-3">
+                                                        <label for="sections" class="form-label">Section(S)</label>
+                                                        <select wire:model='selectedSections' multiple
+                                                            class="form-control" name="sections" id="sections">
+                                                            <option selected>Select one</option>
+                                                            @foreach ($sections as $section)
+                                                                @if (!$section->IsBooked($request->start_time))
+                                                                    <option value="{{ $section->id }}">
+                                                                        {{ $section->name }} -
+                                                                        {{ $section->location ?? '' }}</option>
+                                                                @endif
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
+
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-dismiss="modal">
+                                                        Close
+                                                    </button>
+                                                    <button type="button" class="btn btn-primary"
+                                                        wire:click='makeBooking({{ $request }})'>Save</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-
-
                             </td>
                         </tr>
                     @endforeach
-
-
-
-
                 </tbody>
-                <tfoot>
-
-                </tfoot>
             </table>
         </div>
 
