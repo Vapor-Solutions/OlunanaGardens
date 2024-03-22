70fd9fb77b294f444496583eb2b96b4504f4018b
Errol Onzere Kitsiiri <errolantonio325@gmail.com>
2024-03-22 08:07:20 +0300

    Modified the client booking module

--- app/Livewire/Front/BookingForm.php
+++ app/Livewire/Front/BookingForm.php
@@ -5,122 +5,137 @@ namespace App\Livewire\Front;
 use App\Models\Booking;
 use App\Models\Client;
 use App\Models\EventType;
+use App\Models\Package;
+use App\Models\Section;
 use Carbon\Carbon;
 use Livewire\Component;
 
 class BookingForm extends Component
 {
-
     public $event;
-
-    public $start_time;
-    public $email;
-    // public $initialBooking = true;
     public $bookingIsAvailable = false;
-    public $eventType;
-
-
-    protected $listeners = [
-        'done' => 'render'
-    ];
+    public $noBooking = "james";
 
     protected $rules = [
-        'event.date' => 'required|after_or_equal:today',
+        'event.start_time' => 'required|after_or_equal:today',
+        'event.end_time' => 'required',
         'event.event_type_id' => 'required',
+        'event.package_id' => 'required',
         'event.adults' => 'required',
         'event.children' => 'required',
-        'event.client_name' => 'required',
+        'event.name' => 'required',
+        'event.address' => 'required',
+        'event.phone_number' => 'required',
         'event.email' => 'required|email',
+        'event.price' => 'required',
+        'event.section' => 'required'
     ];
 
     protected $messages = [
-        'event.date' => ['required' => 'The date is required and should not be in the past'],
-        'event.adults' => ['required' => 'The number of adults is required'],
-        'event.event_type_id' => ['required' => 'The event type field is mandatory'],
-        'event.children' => ['required' => 'The number of children is required'],
-        'event.client_name' => ['required' => 'The client\'s name is required'],
-        'event.email' => [
-            'required' => 'The client\'s email address is required',
-            'email' => 'Needs a proper email address format'
-        ],
+        'event.start_time.required' => 'The start date and time is required and should not be in the past',
+        'event.end_time.required' => 'The end date and time is required',
+        'event.event_type_id.required' => 'The event type field is mandatory',
+        'event.package_id.required' => 'The package type is mandatory',
+        'event.section.required' => 'The package type is mandatory',
+        'event.adults.required' => 'The number of adults is required',
+        'event.children.required' => 'The number of children is required',
+        'event.price.required' => 'The price field is required',
+        'event.name.required' => 'The client\'s name is required',
+        'event.address.required' => 'The client\'s address is required',
+        'event.phone_number.required' => 'The client\'s phone number is required',
+        'event.email.required' => 'The client\'s email address is required',
+        'event.email.email' => 'The client\'s email must be a valid email address',
     ];
-
 
     public function checkAvailability()
     {
+        $this->bookingIsAvailable = $this->bookingAvailable();
+        $this->bookingIsAvailable == true ? $this->noBooking = "true":$this->noBooking = "false";
 
-        //validate my inputs
-        // $this->validate();
-        // dd($this->bookingAvailable() , $this->checkClient());
-
-        if ($this->bookingAvailable() && $this->checkClient()) {
-            // proceed with logic to do booking
-            // $this->emit('done', ['success' => 'Date available for booking']);
-
-
-            // $this->initialBooking = true;
-            $this->bookingIsAvailable = true;
-        } else {
-            //else allow user to book
-            // $this->initialBooking = true;
-            $this->bookingIsAvailable = false;
-
-            // $this->emit('done', ['error' => 'Date is already booked']);
-        }
+       
+      
     }
 
 
-    function bookingAvailable(): bool
-    {
+ 
 
+    public function book(){
+        $booking = new Booking();
+        $booking->start_time = $this->event['start_time'];
+        $booking->end_time = $this->event['end_time'];
+        $booking->event_type_id = $this->event['event_type_id'];
+        $booking->package_id = $this->event['package_id'];
+        $booking->capacity_adults = $this->event['adults'];
+        $booking->capacity_children = $this->event['children'];
+        $booking->price = $this->event['price'];
 
-        //convert date chosen by user
-        $choosenDate = Carbon::parse($this->event['date'])->toDate();
-        // dd($choosenDate);
-        // dd($this->event['event_type_id']);
-        // dd($this->eventType);
-
-        //check if a similar booking of the same date and event is available in the database
-        $bookingExists = Booking::where('start_time', $choosenDate)
-            ->where('event_type_id', $this->eventType)->first();
-
-        // dd($bookingExists);
-
-        //if booking exists then user cannot be able to book it
-        if ($bookingExists) {
-            // $this->bookingIs = true;
-            return true;
-        } else {
-            # code...
-            return false;
-        }
-    }
-
-
-
-    //if booking process is going on check whether client exists in the database, if he doesnt prompt him to give his/her details
-
-    public function checkClient(): bool
-    {
         $client = Client::where('email', $this->event['email']);
 
-        // if return is true proceed with booking
-        if ($client->first()) {
-            # code...
-            return true;
-        } else {
-            # code...
-            // create the client
-            
-            return true;
+        if (!$client) {
+            $newClient = new Client();
+            $newClient->name = $this->event['name'];
+            $newClient->email = $this->event['email'];
+            $newClient->phone_number = $this->event['phone_number'];
+            $newClient->address = $this->event['address'];
+            $newClient->save();
         }
+
+        $booking->client_id = $client->id;
+        $booking->save();
+
     }
 
+    public function bookingAvailable(): bool
+    {
+       
+        $eventType = EventType::find($this->event['event_type_id']);
+        // $section = Section::find($this->event['section']);
+        $section = 
+
+        // dd($section);
+
+        $choosenDateStartTime = date_format(Carbon::parse($this->event['start_time']),"Y-m-d H:i:s");
+        $choosenDateEndTime = date_format(Carbon::parse($this->event['end_time']),"Y-m-d H:i:s");
+
+        // dd($choosenDateStartTime , $choosenDateEndTime);
+
+        $bookingExists = Booking::
+            whereBetween('start_time', [
+                $choosenDateStartTime,
+                $choosenDateEndTime])
+            ->where('event_type_id', $eventType->id)
+            ->where('section', $section)
+            ->first();
+        dd($bookingExists);
+
+        
+        return $bookingExists ? true : false;
+    }
+
+    // public function checkClient(): bool
+    // {
+    //     $client = Client::where('email', $this->event['email'])->first();
+
+    //     if ($client) {
+    //         return true;
+    //     } else {
+    //         $newClient = new Client();
+    //         $newClient->name = $this->event['name'];
+    //         $newClient->email = $this->event['email'];
+    //         $newClient->phone_number = $this->event['phone_number'];
+    //         $newClient->address = $this->event['address'];
+    //         $newClient->save();
+
+    //         return true;
+    //     }
+    // }
 
     public function render()
     {
         return view('livewire.front.booking-form', [
-            'eventTypes' => EventType::all()
+            'eventTypes' => EventType::all(),
+            'packageTypes' => Package::all(),
+            'sections' => Section::all(),
         ]);
     }
 }
--- resources/views/livewire/front/booking-form.blade.php
+++ resources/views/livewire/front/booking-form.blade.php
@@ -1,4 +1,4 @@
-<div>
+{{-- <div>
     <div class="booking-box">
         <div class="head-box">
             <h4>Booking Form</h4>
@@ -6,22 +6,49 @@
         <div class="booking-inner clearfix">
             <div class="form1 clearfix">
                 <div class="row">
-
-
-
-
                     <div class="col-md-12">
-                        {{-- @if ($bookingIsAvailable) --}}
+                        @if ($bookingIsAvailable)
                             <div class="col-md-12">
-
                                 <div class="col-12">
                                     <div class="mb-3 input1_inner">
                                         <label for="" class="form-label">Name</label>
                                         <input type="text" class="form-control" name="" id=""
                                             aria-describedby="helpId" placeholder="Enter Your Name"
-                                            wire:model='event.client_name'>
-                                        @error('event.client_name')
+                                            wire:model='event.name'>
+                                        @error('event.name')
                                             <small class="text-danger">{{ $message }}</small>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-12">
+                                    <div class="mb-5">
+                                        <label for="phone_number" class="form-label">Phone Number</label>
+                                        <input wire:model="client.phone_number" type="text" name="phone_number" id=""
+                                            class="form-control" placeholder="Enter Client's Phone Number" aria-describedby="name">
+                                        <small id="phone_number" class="text-muted">Enter Client's Phone Number</small><br>
+                                        @error('client.phone_number')
+                                            <small id="phone_number" class="text-danger">{{ $message }}</small>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6 col-12">
+                                    <div class="mb-5">
+                                        <label for="country" class="form-label">Country/Nationality</label>
+                                        <input wire:model="client.country" type="text" name="country" id=""
+                                            class="form-control" placeholder="Enter Client's Country" aria-describedby="name">
+                                        <small id="country" class="text-muted">Enter Client's Residing Country</small><br>
+                                        @error('client.country')
+                                            <small id="country" class="text-danger">{{ $message }}</small>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="mb-5">
+                                        <label for="address" class="form-label">Physical Address</label>
+                                        <textarea wire:model='client.address' placeholder="Enter Client's Physical Address" class="form-control" name="address2" id="address2" rows="2"></textarea>
+                                        @error('client.address')
+                                            <small id="address2" class="text-danger">{{ $message }}</small>
                                         @enderror
                                     </div>
                                 </div>
@@ -38,7 +65,6 @@
                                         </div>
                                     </div>
                                 </div>
-
                                 <div class="col-md-6">
                                     <div class="mb-3 input1_inner">
                                         <label>Children</label>
@@ -50,51 +76,50 @@
                                         @enderror
                                     </div>
                                 </div>
-
-
                             </div>
-                        {{-- @endif --}}
-                        
-                            <div class="">
-                                <div class="mb-3 input1_inner">
-                                    <label for="startDate" class="form-label">Booking Date</label>
-                                    <input type="datetime-local" wire:model="event.date"
-                                        min="{{ Carbon\Carbon::now()->addDay()->toDateTimeString() }}"
-                                        class="form-control" name="startDate" id="startDate"
-                                        aria-describedby="startDate">
-                                    @error('event.date')
-                                        <small class="text-danger">{{ $message }}</small>
-                                    @enderror
-                                </div>
-                            </div>
+                        @endif
 
-                            <div class="mb-3">
-                                <select wire:model="eventType" class="form-control" required>
-                                    <option value="">Choose the event type</option>
-                                    @foreach ($eventTypes as $type)
-                                        <option value="{{ $type->id }}">{{ $type->title }}</option>
-                                    @endforeach
-                                </select>
+                        <div class="">
+                            <div class="mb-3 input1_inner">
+                                <label for="startDate" class="form-label">Booking Date</label>
+                                <input type="datetime-local" wire:model="event.start_time"
+                                    min="{{ Carbon\Carbon::now()->addDay()->toDateTimeString() }}" class="form-control"
+                                    name="startTime" id="startTime" aria-describedby="startTime">
+                                @error('event.start_time')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
                             </div>
+                        </div>
 
+                        <div class="mb-3">
+                            <select wire:model="event.event_type_id" class="form-control" required>
+                                <option value="">Choose the event type</option>
+                                @foreach ($eventTypes as $type)
+                                    <option value="{{ $type->id }}">{{ $type->title }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <select wire:model="event.event_type_id" class="form-control" required>
+                                <option value="">Choose your package</option>
+                                @foreach ($packageTypes as $package)
+                                    <option value="{{ $package->id }}">{{ $package->title }}</option>
+                                @endforeach
+                            </select>
+                        </div>
 
-                            <div class="col-12">
-                                <div class="mb-3 input1_inner">
-                                    <label for="" class="form-label">Email Address</label>
-                                    <input type="text" class="form-control" name="" id=""
-                                        aria-describedby="helpId" placeholder="Enter Your Email Address"
-                                        wire:model='event.email'>
-                                    @error('event.email')
-                                        <small class="text-danger">{{ $message }}</small>
-                                    @enderror
-                                </div>
+                        <div class="col-12">
+                            <div class="mb-3 input1_inner">
+                                <label for="" class="form-label">Email Address</label>
+                                <input type="text" class="form-control" name="" id=""
+                                    aria-describedby="helpId" placeholder="Enter Your Email Address"
+                                    wire:model='event.email'>
+                                @error('event.email')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
                             </div>
+                        </div>
                     </div>
-
-
-
-
-
                     @if ($bookingIsAvailable)
                         <div class="col-md-12">
                             <button wire:click='book' class="btn-form1-submit mt-15">Book now
@@ -111,3 +136,160 @@
             </div>
         </div>
     </div>
+</div> --}}
+
+<div>
+    <div class="booking-box">
+        <div class="head-box">
+            <h4>Booking Form</h4>
+        </div>
+        <div class="booking-inner clearfix">
+            <div class="form1 clearfix">
+            
+                @if ($noBooking)
+                    <p class="text-danger">no booking</p>
+                @endif
+                @if ($bookingIsAvailable)
+                    {{-- give  me a booking form --}}
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="mb-3 input1_inner">
+                                <label for="" class="form-label">Name</label>
+                                <input type="text" class="form-control" name="name" id="name"
+                                    aria-describedby="helpId" placeholder="Enter Your Name" wire:model='event.name'>
+                                @error('event.name')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                            <div class="mb-3 input1_inner">
+                                <label for="phone_number" class="form-label">Phone Number</label>
+                                <input type="text" class="form-control" name="phone_number" id="phone_number"
+                                    aria-describedby="helpId" placeholder="Enter Your Phone Number"
+                                    wire:model='event.phone_number'>
+                                @error('event.phone_number')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+
+                            <div class="col-12">
+                                <div class="mb-3 input1_inner">
+                                    <label for="" class="form-label">Email Address</label>
+                                    <input type="text" class="form-control" name="" id=""
+                                        aria-describedby="helpId" placeholder="Enter Your Email Address"
+                                        wire:model='event.email'>
+                                    @error('event.email')
+                                        <small class="text-danger">{{ $message }}</small>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <div class="mb-3 input1_inner">
+                                    <label for="country" class="form-label">Country/Nationality</label>
+                                    <input wire:model="client.country" type="text" name="country" id=""
+                                        class="form-control" placeholder="Enter Client's Country"
+                                        aria-describedby="name">
+                                    <small id="country" class="text-muted">Enter Client's Residing Country</small><br>
+                                    @error('client.country')
+                                        <small id="country" class="text-danger">{{ $message }}</small>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <div class="mb-3 input1_inner">
+                                    <label for="address" class="form-label">Physical Address</label>
+                                    <textarea wire:model='client.address' placeholder="Enter Client's Physical Address" class="form-control" name="address2"
+                                        id="address2" rows="2"></textarea>
+                                    @error('client.address')
+                                        <small id="address2" class="text-danger">{{ $message }}</small>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="mb-3 input1_inner">
+                                    <div class=" mb-3 input1_inner">
+                                        <label>Adults</label>
+                                        <input type="number" wire:model='event.adults' class="form-control "
+                                            placeholder="Adults">
+                                        @error('event.adults')
+                                            <small class="text-danger">{{ $message }}</small>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="mb-3 input1_inner">
+                                    <label>Children</label>
+                                    <input type="number" class="form-control" name="" id=""
+                                        aria-describedby="helpId" placeholder="Children" wire:model='event.children'>
+                                    @error('event.children')
+                                        <small class="text-danger">{{ $message }}</small>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="mb-3 input1_inner">
+                                <select wire:model="event.event_type_id" class="form-control" required>
+                                    <option value="">Choose your package</option>
+                                    @foreach ($packageTypes as $package)
+                                        <option value="{{ $package->id }}">{{ $package->title }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    @if ($bookingIsAvailable)
+                        <button wire:click="book" class="btn-form1-submit mt-15">Book Now</button>
+                    @endif
+                @else
+                    {{-- form to check for availability of a booking --}}
+                 
+                    <div class="mb-3 input1_inner">
+                        <label for="startDate" class="form-label">Booking Date</label>
+                        <input type="datetime-local" wire:model="event.start_time"
+                            min="{{ Carbon\Carbon::now()->addDay()->toDateTimeString() }}" class="form-control"
+                            name="startTime" id="startTime" aria-describedby="startTime">
+                        @error('event.start_time')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="mb-3 input1_inner">
+                        <label for="End Date" class="form-label">Booking Date</label>
+                        <input type="datetime-local" wire:model="event.end_time"
+                            min="{{ Carbon\Carbon::now()->addDay()->toDateTimeString() }}" class="form-control"
+                            name="startTime" id="startTime" aria-describedby="startTime">
+                        @error('event.end_time')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <select wire:model="event.event_type_id" class="form-control" required>
+                            <option value="">Choose the event type</option>
+                            @foreach ($eventTypes as $type)
+                                <option value="{{ $type->id }}">{{ $type->title }}</option>
+                            @endforeach
+                        </select>
+                        @error('event.event_type_id')
+                        <small class="text-danger">{{ $message }}</small>
+                    @enderror
+                    </div>
+                    <div class="mb-3">
+                        <select wire:model="event.section" class="select2" required>
+                            <option value="">Choose your section</option>
+                            @foreach ($sections as $section)
+                                <option value="{{ $section->id }}">{{ $section->name }}</option>
+                            @endforeach
+                        </select>
+                        @error('event.section')
+                        <small class="text-danger">{{ $message }}</small>
+                    @enderror
+                    </div>
+                    <button wire:click="checkAvailability" class="btn-form1-submit mt-15">Check Availability</button>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
